ef60e020084d47880ce1816a4533a9a624b59387
Vinnikov Alexandr <makalex_90@mail.ru>
2013-02-18 11:47:14 +0700

    Добавил еще файликов

---
+++ src/Forwarder/ForwarderRepository/Entity/FRDbContect.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ForwarderRepository.Entity
+{
+    class FRDbContect : DbContext{
+        public DbSet<Object> Object { get; set; }
+    }
+}
