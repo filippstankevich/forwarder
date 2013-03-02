109105c109da76d13fcacb12ea0663ba84e33d65
Vinnikov Alexandr <makalex_90@mail.ru>
2013-03-03 01:55:41 +0700

    Поправил роутинг.

---
+++ src/Forwarder/Forwarder/Models/TestModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Forwarder.Models
+{
+    public class TestModel
+    {
+        [Required(ErrorMessage="Enter name")]
+        [Display(Name="Test field")]
+        public string Name = "Тестовая строка";
+
+        [Display(Name = "Success???")]
+        public string Result;
+    }
+}
