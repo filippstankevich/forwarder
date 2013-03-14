78e9654c27779895a3db8e902c62343ce2ca4d93
Evgen1991 <7evgen7@sibmail.com>
2013-03-14 21:27:46 +0200

    Построение JQGrid

---
+++ src/Forwarder/Forwarder/Controllers/GridController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forwarder.Controllers
+{
+    public class GridController : Controller
+    {
+        //
+        // GET: /Grid/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GridView()
+        {
+
+            var list = new List<GridModel>
+
+                           {
+
+                               new GridModel{Id = 1, FirstName = "Александр", LastName = "Македонский", Email = "alex.mak@test.com"},
+
+                               new GridModel {Id = 2, FirstName = "Иван", LastName = "Петров", Email = "ivan@test.com"},
+
+                               new GridModel{Id = 3, FirstName = "Сергей", LastName = "Сидоров", Email = "sarg@test.com"},
+
+                           };
+
+
+
+            var result = new JsonResult()
+
+            {
+
+                Data = new { page = 0, total = 1, records = list.Count, rows = list }
+
+            };
+
+
+            result.JsonRequestBehavior = JsonRequestBehavior.AllowGet;
+            return result;
+
+        }
+
+        public class GridModel
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Email { get; set; }
+
+        }
+
+    }
+}
