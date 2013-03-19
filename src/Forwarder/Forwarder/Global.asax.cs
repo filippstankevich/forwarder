57d37fc04c0058d49bd4278efd696738f1688048
Vinnikov Alexandr <makalex_90@mail.ru>
2013-03-19 23:25:57 +0700

    Добавил немного библиотек. и поправил начальную страницу

--- src/Forwarder/Forwarder/Global.asax.cs
+++ src/Forwarder/Forwarder/Global.asax.cs
@@ -24,7 +24,7 @@ namespace Forwarder
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Grid", action = "GridView", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Grid", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
         }
 
