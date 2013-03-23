74f2167f847e1f226f7c41db94d556a063778d4c
Evgen1991 <7evgen7@sibmail.com>
2013-03-23 23:50:15 +0200

    Исправление внешнего вида грида, задание в контроллере входных параметров для поиска.

--- src/Forwarder/Forwarder/Controllers/GridController.cs
+++ src/Forwarder/Forwarder/Controllers/GridController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using ForwarderDAL.Entity;
 
+
+
 namespace Forwarder.Controllers
 {
     public class GridController : Controller
@@ -24,8 +26,15 @@ namespace Forwarder.Controllers
 
         public JsonResult GridView()
         {
+            string search = Request.Params["_search"]; //Булево значение, если запрос с условием поиска оно принимает истинное значение
+            string filters =  Request.Params["filters"]; // Объект с условиями в представлении json;
+            string searchField =  Request.Params["searchField"]; // Имя поля для поиска (если условие простое);
+            string searchOper =  Request.Params["searchOper"]; //Операция сравнения поля для поиска (если условие простое);
+            string searchString = Request.Params["searchString"]; // Значение поля для поиска (если условие простое);
+            
             var list = new List<object>
                 {
+                    
                     new {
                             id = 1,
                             cell = new string[]
@@ -41,9 +50,43 @@ namespace Forwarder.Controllers
                                     "ок",
                                     "1.01.2012"
                                 }
+                    },
+                    new {
+                            id = 2,
+                            cell = new string[]
+                               {
+                                    "2",
+                                    "12345",
+                                    "новосиб",
+                                    "Москва",
+                                    "32323",
+                                    "321",
+                                    "9",
+                                    "100",
+                                    "ок",
+                                    "2.01.2012"
+                                }
+                    },
+                    new {
+                            id = 3,
+                            cell = new string[]
+                               {
+                                    "3",
+                                    "12345",
+                                    "Красноярск",
+                                    "Москва",
+                                    "32323",
+                                    "321",
+                                    "10",
+                                    "200",
+                                    "ок",
+                                    "3.01.2012"
+                                }
                     }
                 };
 
+           
+           
             var result = new JsonResult()
             {
                 Data = new
