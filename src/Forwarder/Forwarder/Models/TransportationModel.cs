e6b56de42ff1de9f0c4767cbd3a000090eb8b317
Filipp <f.stankevich@mail.ru>
2013-04-18 22:56:35 +0700

    Добавил TransportationModel, поля для Перевозки

---
+++ src/Forwarder/Forwarder/Models/TransportationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Forwarder.Models
+{
+    public class TransportationModel
+    {
+        public IEnumerable<SelectListItem> GngItems { get; set; }
+        public IEnumerable<SelectListItem> EtsngItems { get; set; }
+        public IEnumerable<SelectListItem> StationItems { get; set; }
+    }
+}
